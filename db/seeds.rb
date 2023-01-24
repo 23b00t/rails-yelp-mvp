491f58e13dd0fdb152c0f1e725ecf729cb8aa954
Daniel Kipp <daniel.kipp@gmail.com>
2023-01-24 21:03:39 +0100

    functionality is working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create!(name: 'don', address: 'italy', phone_number: '12345', category: 'italian')
+Restaurant.create!(name: 'gunner', address: 'berlin', phone_number: '365633', category: 'japanese')
+Restaurant.create!(name: 'dinas', address: 'rom', phone_number: '77777', category: 'french')
+Restaurant.create!(name: 'erwins', address: 'heer', category: 'italian')
+Restaurant.create!(name: 'super', address: 'local road', phone_number: '888888', category: 'italian')
